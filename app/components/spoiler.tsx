1cf9371dfe6f8cd718861ebbcf99745aa4212ff6
Ly <echo.aimless@gmail.com>
2026-04-14 21:43:11 +0800

    chore: spoiler support multiline

--- app/components/spoiler.tsx
+++ app/components/spoiler.tsx
@@ -9,20 +9,16 @@ export function Spoiler({ children, className, ...props }: SpoilerProps) {
     <span
       tabIndex={0}
       className={[
-        'group relative isolate inline-block align-baseline text-inherit focus-visible:outline-none',
+        'group box-decoration-clone inline rounded-[0.16em] bg-current px-[0.08em] align-baseline text-inherit transition-colors duration-150 ease-out hover:bg-transparent focus-visible:bg-transparent focus-visible:outline-none',
         className,
       ]
         .filter(Boolean)
         .join(' ')}
       {...props}
     >
-      <span className="relative z-0 inline-block px-[0.08em] text-transparent transition-colors duration-150 ease-out group-hover:text-inherit group-focus-visible:text-inherit">
+      <span className="text-transparent transition-colors duration-150 ease-out group-hover:text-inherit group-focus-visible:text-inherit">
         {children}
       </span>
-      <span
-        aria-hidden="true"
-        className="pointer-events-none absolute inset-x-0 top-[0.18em] bottom-[0.16em] z-[1] rounded-[0.16em] bg-current transition-opacity duration-150 ease-out group-hover:opacity-0 group-focus-visible:opacity-0"
-      />
     </span>
   )
 }
