972f6a669fd96463159a673fb2e555b11c33738d
Ly <echo.aimless@gmail.com>
2026-04-14 11:17:08 +0800

    chore: github button stubs

--- app/lib/layout.shared.tsx
+++ app/lib/layout.shared.tsx
@@ -1,10 +1,11 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
-import { appName } from './shared'
+import { appName, githubUrl } from './shared'
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: appName,
     },
+    githubUrl,
   }
 }
--- app/lib/shared.ts
+++ app/lib/shared.ts
@@ -1,3 +1,4 @@
 export const appName = 'HZNU OS Lab'
 export const docsRoute = '/docs'
 export const docsContentRoute = '/docs-content'
+export const githubUrl = 'https://github.com'
